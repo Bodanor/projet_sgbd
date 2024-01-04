0ca2aabd02b416d99e78b8235b98b48fd466abe5
Bodanor <christos.papadopoulos@student.hepl.be>
2024-01-04 11:49:10 -0500

    Second version

--- src/GetMotionInterval.java
+++ src/GetMotionInterval.java
@@ -1,2 +1,29 @@
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import java.util.ArrayList;
+
 public class GetMotionInterval {
+
+    private final ArrayList<Motion> listMotion = new ArrayList<Motion>();
+    public GetMotionInterval()
+    {
+        String serverIp = "";
+    }
+    public GetMotionInterval(String ip, int startingInterval) throws JsonProcessingException {
+        JSONArray received = GetJSON.init("http://" + ip + ":8080/ords/papabergh/demo/motion?limit=120&beginsample=" +(startingInterval) + "&lastsample=" + (startingInterval + 119), "items");
+        for(int i = 0; i < received.length(); i++)
+        {
+            JSONObject current_object = new JSONObject(received.get(i).toString());
+            Motion motion_temp = new ObjectMapper().readValue(current_object.toString(), Motion.class);
+            listMotion.add(motion_temp);
+        }
+
+    }
+    public Motion get(int index)
+    {
+        return this.listMotion.get(index);
+    }
 }
+
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.json.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws JsonProcessingException {
+
+       GetMotionInterval motion = new GetMotionInterval("192.168.0.18", 1);
     }
 }
