e67a2c3822e9d653a032132f5830720f3a481a92
Bodanor <christos.papadopoulos@student.hepl.be>
2024-01-05 07:27:04 -0500

    Changes by martin

--- src/GUI.java
+++ src/GUI.java
@@ -3,6 +3,7 @@ import javax.swing.*;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
+import org.jfree.chart.ChartUtils;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.data.category.DefaultCategoryDataset;
@@ -11,10 +12,19 @@ import org.jfree.data.general.DefaultPieDataset;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.*;
 import java.util.ArrayList;
 
 import static java.lang.Integer.parseInt;
 import java.util.Timer;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.Base64;
+
+import org.jfree.chart.ChartUtils;
+import org.json.JSONObject;
 
 public class GUI extends JFrame{
     private JPanel panel1;
@@ -83,7 +93,42 @@ public class GUI extends JFrame{
 
             }
         });
+        buttonscreenshot.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    File fi = new File("");
+                    ChartUtils.saveChartAsJPEG(fi, jfc, cp.getWidth(), cp.getHeight());
+                    byte[] byteArray = Files.readAllBytes(fi.toPath());
+                    String encoded = Base64.getEncoder().encodeToString(byteArray);
+                    JSONObject json = new JSONObject();
+                    json.put("image", encoded);
+                    String urlParameters = json.toString();
+                    byte[] postData = urlParameters.getBytes(StandardCharsets.UTF_8 );
+                    int postDataLength = postData.length;
+                    URL url = new URL("http://localhost:8080/ords/hr/demo/blob");
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    conn.setDoOutput(true);
+                    conn.setInstanceFollowRedirects(false);
+                    conn.setRequestMethod("POST"); //Verbe POST
+                    conn.setRequestProperty("Content-Type", "application/json");
+                    conn.setRequestProperty("charset", "utf-8");
+                    conn.setRequestProperty("Content-Length", Integer.toString(postDataLength));
+                    conn.setUseCaches(false);
+                    DataOutputStream wr = new DataOutputStream(conn.getOutputStream());
+                    wr.write(postData);
+                    BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                    String line;
+                    while ((line = rd.readLine()) != null) {
+                        System.out.println(line);
+                    }
+                    rd.close();
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
 
+            }
+        });
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setSize(800, 600);
         setLocationRelativeTo(null);
